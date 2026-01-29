f8abaca7634ae88650e3ef6921a4bf783650d69f
Maewarr <119325874+ParthivRajesh@users.noreply.github.com>
2026-01-29 12:53:35 +0530

    Notebook 2

---
+++ notebook 2.py
@@ -0,0 +1,37 @@
+# Write a program that
+# 1. Stores your name in a variable
+# 2. Stores your favorite organism in another variable
+# 3. Print both using print()
+
+name = "Parthiv"
+organism = " Bacillus-Subtilis"
+
+print(name)
+print(organism)
+
+
+# Write a program that stores the value of pi in a variable
+# 1. Stores the number 5 in a variable
+# 2. Stores the number 10 in another variable
+# 3. Prints both numbers
+# 4. Performs both subtraction and addition calculations
+
+num1 = 5
+num2 = 10
+addition = num1 + num2
+subtraction = num2 - num1
+
+print(addition)
+print(subtraction)
+
+
+
+# Write a program that takes 3 numbers and prints the average
+
+name = "Parthiv"
+num3 = 5
+num4 = 6
+num5 = 7
+
+average = (num3 + num4 + num5)/3
+print(average)
