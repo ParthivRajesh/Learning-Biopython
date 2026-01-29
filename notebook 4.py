7a3e2433ab4b6e3afb8b6f54c656b451438b3d1c
Maewarr <119325874+ParthivRajesh@users.noreply.github.com>
2026-01-29 12:54:15 +0530

    Notebook 4

---
+++ notebook 4.py
@@ -0,0 +1,34 @@
+# Practise Questions
+## 1
+
+name = "Parthiv Rajesh"
+seq1 = "ATGCTAGGCTA"
+
+print(seq1)
+print(type(seq1))
+
+## 2
+
+name = "Parthiv Rajesh"
+seq2 = "ATGCGTACGTTAGC"
+print(len(seq2))
+
+## 3
+
+name = "Parthiv Rajesh"
+seq3 = "ATGCGTAC"
+
+print(seq3[0])
+print(seq3[2])
+print(seq3[7])
+
+
+## 4
+
+name = "Parthiv Rajesh"
+dna1 = "ATGCGT"
+dna2 = "ATGCGTAGG"
+
+print("The length of dna1: ", len(dna1))
+print("The length of dna2: ", len(dna2))
+print("Nucleotide at Pos 5: ", dna2[4])
