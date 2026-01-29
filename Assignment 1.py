4cffd48e8ad8f3bfeeb72ac85b0fe3478f9f10e5
Maewarr <119325874+ParthivRajesh@users.noreply.github.com>
2026-01-29 13:05:24 +0530

    Assignment 1

---
+++ Assignment 1.py
@@ -0,0 +1,41 @@
+# PART B
+
+# Q4
+dna = "DNA"
+rna = "RNA"
+protein = "PROTEIN"
+
+print(dna)
+print(rna)
+print(protein)
+
+# Q5
+student_name = "Parthiv Rajesh"
+print(student_name)
+
+# Q6
+x = 10
+y = 5
+sum = x + y
+print(sum)
+
+# Q7
+riya = "12345678901"
+print(riya)
+
+
+# PART C
+
+# Q8
+dna_sequence = "ATGCATGCTACT"
+print(dna_sequence)
+
+# Q9
+gene = "TP53"
+print(gene)
+
+# Optional Exercise
+
+z = 3
+print(z)
+
